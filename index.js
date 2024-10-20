7d9926ad44535f983d3af1f78e851d765f60a3e5
Sinchana-Rai <sinchana.srai@gmail.com>
2024-10-20 10:49:50 -0400

    js file initial commit

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-import "./index.css";
+//import "./index.css";
+
+
+
+const inputForm = document.getElementById("form");
+const addButton = document.getElementById("add");
+const birthdayList = document.getElementById("birthdayList");
+
+
+const newText = document.createElement("h3");
+newText.innerText = "Never Forget any birthdays and plan ahead of the occasion";
+newText.style.color = "white";
+
+
+const headerText = document.querySelector(".heading");
+headerText.insertBefore(newText, headerText.firstChild);
+
+
+// alert("hello")
+
+inputForm.addEventListener("submit", (e) => {
+  // alert("hi")
+  e.preventDefault();
+   
+    const personName = inputForm.querySelector('input[name="name"]').value.trim();
+  if (personName === "") {
+    // alert("Please enter name"); 
+    return;
+  }
+
+ 
+
+});
+
